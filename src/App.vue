a2cbba3c231febbc2a59d261bf2809c961b96f34
zwhy2024 <thecvcoder@foxmail.com>
2024-07-16 10:44:19 +0800

    sort menu

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const sortedRoutes = computed(() =>
+  Array.from(router.options.routes).sort(
+    (a, b) => (a.meta?.menuOrder as number) - (b.meta?.menuOrder as number)
+  )
+)
+</script>
+
 <template>
   <div class="container">
-    <h1 class="title">Vue Dynamic Config</h1>
+    <h1 class="title">Vue Dynamic Router Config</h1>
     <ol class="navbar">
-      <RouterLink to="/">首页</RouterLink>
-      <RouterLink to="/about">关于</RouterLink>
-      <RouterLink to="/products">产品</RouterLink>
-      <RouterLink to="/news">新闻</RouterLink>
+      <RouterLink v-for="link of sortedRoutes" :to="link.path">
+        {{ link.meta?.title }}
+      </RouterLink>
     </ol>
-    <div class="content">
-      <RouterView />
-    </div>
+    <RouterView />
   </div>
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
 .container {
-  width: fit-content;
-  margin: 0 auto;
-}
-
-.container .title {
-  margin-bottom: 16px;
-}
-
-.container .navbar {
+  width: 100dvw;
+  height: 100%;
+  min-height: 100dvh;
   display: flex;
-  gap: 24px;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 128px;
+  row-gap: 24px;
 }
 
+.navbar {
+  display: flex;
+  gap: 16px;
+}
 </style>
